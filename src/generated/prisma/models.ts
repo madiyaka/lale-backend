bd1c2f6c1afca3d53d8f0e51fb533eee4d5dec34
madiyaka <wardston2@gmail.com>
2026-04-25 15:00:33 +0500

    switch to commonjs without adapter

--- src/generated/prisma/models.ts
+++ src/generated/prisma/models.ts
@@ -8,8 +8,8 @@
  *
  * 🟢 You can import this file directly.
  */
-export type * from './models/User.js'
-export type * from './models/WaterBody.js'
-export type * from './models/WaterBodyPassport.js'
-export type * from './models/BioindicationRecord.js'
-export type * from './commonInputTypes.js'
+export type * from './models/User'
+export type * from './models/WaterBody'
+export type * from './models/WaterBodyPassport'
+export type * from './models/BioindicationRecord'
+export type * from './commonInputTypes'
